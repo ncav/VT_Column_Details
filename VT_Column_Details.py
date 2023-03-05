4a85f4680f89ad2f3b7392b3836ccd2111051cef
Sneaky-Sec <52970090+ncav@users.noreply.github.com>
2023-03-04 22:49:41 -0500

    Create VT_Column_Details.py

---
+++ VT_Column_Details.py
@@ -0,0 +1,33 @@
+import requests
+
+api_key = ""
+
+def get_detection_results(api_key, resource):
+    url = f"https://www.virustotal.com/api/v3/files/{resource}/analyzes"
+    headers = {"x-apikey": api_key}
+    params = {"limit": 1}
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        response_json = response.json()
+        analysis_id = response_json["data"][0]["id"]
+        analysis_url = f"https://www.virustotal.com/gui/file/{resource}/detection/{analysis_id}"
+        detections = response_json["data"][0]["attributes"]["last_analysis_results"]
+        return detections, analysis_url
+    else:
+        return None, None
+
+resource = '2935a7d27440cb3e1105dc4e88c8f87f36a5c40cbf52859c74de54b98ab5af4e'
+detections, analysis_url = get_detection_results(api_key, resource)
+
+if detections is not None:
+    print("Antivirus             Detected  Version               Result                             Update")
+    print("---------             --------  -------               ------                             ------")
+    for vendor, data in detections.items():
+        detected = data.get("detected", False)
+        version = data.get("version", "")
+        result = data.get("result", "")
+        update = data.get("update", "")
+        print(f"{vendor:<20} {detected:<9} {version:<20} {result:<35} {update}")
+    print(f"\nFull analysis report: {analysis_url}")
+else:
+    print("Error retrieving detection results")
